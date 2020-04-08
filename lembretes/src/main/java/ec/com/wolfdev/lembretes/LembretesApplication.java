97686a5b55a5d9c9b4504e032724d8febc5dac88
Alex Villa <alex22_w@hotmail.com>
2020-04-08 15:33:29 -0500

    create back lembretes-ws
    back end Spring has been created

---
+++ lembretes/src/main/java/ec/com/wolfdev/lembretes/LembretesApplication.java
@@ -0,0 +1,13 @@
+package ec.com.wolfdev.lembretes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembretesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembretesApplication.class, args);
+	}
+
+}
---
+++ lembretes/src/test/java/ec/com/wolfdev/lembretes/LembretesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.wolfdev.lembretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
